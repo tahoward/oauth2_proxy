f6624989fb1dc28dc440ba59cfea9a056ab23f22
Alik Khilazhev <alikhil@mail.ru>
2018-05-16 18:44:25 +0300

    Finish applying changes from prev commit

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flagSet := flag.NewFlagSet("oauth2_proxy", flag.ExitOnError)
 
+	whitelistDomains := StringArray{}
 	emailDomains := StringArray{}
 	upstreams := StringArray{}
 	skipAuthRegex := StringArray{}
@@ -42,6 +43,7 @@ func main() {
 	flagSet.Bool("skip-auth-preflight", false, "will skip authentication for OPTIONS requests")
 	flagSet.Bool("ssl-insecure-skip-verify", false, "skip validation of certificates presented when using HTTPS")
 
+	flagSet.Var(&whitelistDomains, "whitelist-domain", "allowed domains for redirection after authentication")
 	flagSet.Var(&emailDomains, "email-domain", "authenticate emails with the specified domain (may be given multiple times). Use * to authenticate any email")
 	flagSet.String("azure-tenant", "common", "go to a tenant-specific or common (tenant-independent) endpoint.")
 	flagSet.String("github-org", "", "restrict logins to members of this organisation")
